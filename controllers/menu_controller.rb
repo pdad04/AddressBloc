f797921a18aa310992938a63efb93bd4622429b8
Andre <andre_dean@me.com>
2017-11-25 19:54:33 -0800

    implement functionality to delete all addressbook entries

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - Nuke Address Book (Delete all entries)"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -36,6 +37,10 @@ class MenuController
         read_csv
         main_menu
       when 5
+        system"clear"
+        delete_all_entries
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -139,6 +144,21 @@ class MenuController
   def delete_entry(entry)
     address_book.entries.delete(entry)
     puts "#{entry.name} has been deleted"
+  end
+
+  def delete_all_entries
+    puts "Do you really want to deleted ALL entries? This action CANNOT be undone!"
+    print "Enter 'Y' or 'N': "
+    input = gets.chomp.downcase
+
+    if input == 'y'
+      address_book.entries.clear
+      system "clear"
+      main_menu
+    end
+
+    system "clear"
+    main_menu
   end
 
   def edit_entry(entry)
